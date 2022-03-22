015f0925d0a2a84201fcfcd2bf3104fb0dc1787c
FERNANDO-BAUTISTA-CASDADO <99769697+FERNANDO-BAUTISTA-CASDADO@users.noreply.github.com>
2022-03-22 16:59:24 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,1 +1,13 @@
+<html>
+<body>
 
+Welcome <?php echo $_POST["name"]; ?><br>
+Tu nombre es: <?php echo $_POST["Nombres"]; ?>
+<?php echo $_POST["name"]; ?><br>
+Tus apellidos son: <?php echo $_POST["Apellidos"]; ?>
+<?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+ <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["date|"]; ?>
+</body>
+</html>
